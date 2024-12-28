e8a543a8e28d8fef788389a016320006aa57f205
mikosa <sabbagh.mike@gmail.com>
2024-12-28 11:41:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 """
-Example for a BLE 4.0 Server using a GATT dictionary of services and     
-characteristics
+Example for a BLE 4.0 Server
 """
 import sys
 import logging
 import asyncio
 import threading
 
-from typing import Any, Dict, Union
+from typing import Any, Union
 
 from bless import (  # type: ignore
     BlessServer,
@@ -19,6 +18,7 @@ from bless import (  # type: ignore
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(name=__name__)
 
+# NOTE: Some systems require different synchronization methods.
 trigger: Union[asyncio.Event, threading.Event]
 if sys.platform in ["darwin", "win32"]:
     trigger = threading.Event()
@@ -35,66 +35,49 @@ def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs)
     characteristic.value = value
     logger.debug(f"Char value set to {characteristic.value}")
     if characteristic.value == b"\x0f":
-        logger.debug("Nice")
+        logger.debug("NICE")
         trigger.set()
 
 
 async def run(loop):
     trigger.clear()
-
     # Instantiate the server
-    gatt: Dict = {
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD": {
-            "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B": {
-                "Properties": (
-                    GATTCharacteristicProperties.read
-                    | GATTCharacteristicProperties.write
-                    | GATTCharacteristicProperties.indicate
-                ),
-                "Permissions": (
-                    GATTAttributePermissions.readable
-                    | GATTAttributePermissions.writeable
-        "5c339364-c7be-4f23-b666-a8ff73a6a86a": {
-            "bfc0c92f-317d-4ba9-976b-cc11ce77b4ca": {
-                "Properties": GATTCharacteristicProperties.read,
-                "Permissions": GATTAttributePermissions.readable,
-                "Value": bytearray(b"\x69"),
-            }
-        },
-    }
-    my_service_name = "Hack me Service"
+    my_service_name = "Test Service"
     server = BlessServer(name=my_service_name, loop=loop)
     server.read_request_func = read_request
     server.write_request_func = write_request
 
-    await server.add_gatt(gatt)
-    # Set manufacturer data for advertising
-    battery_level=80
-    server.manufacturer_data = {
-        1: bytearray([battery_level])  # Manufacturer ID 1, data is battery level
-    }
-    await server.start()
-    logger.debug(server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"))
-    logger.debug("Advertising")
-    logger.info(
-        "Write '0xF' to the advertised characteristic: "
-        + "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+    # Add Service
+    my_service_uuid = "00002a05-0000-1000-8000-00805f9b34fb"
+    await server.add_new_service(my_service_uuid)
+
+    # Add a Characteristic to the service
+    my_char_uuid = "00002b29-0000-1000-8000-00805f9b34fb"
+    char_flags = (
+        GATTCharacteristicProperties.read
+        | GATTCharacteristicProperties.write
+        | GATTCharacteristicProperties.indicate
     )
+    permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
+    await server.add_new_characteristic(
+        my_service_uuid, my_char_uuid, char_flags, None, permissions
+    )
+
+    logger.debug(server.get_characteristic(my_char_uuid))
+    await server.start()
+    logger.debug("Advertising")
+    logger.info(f"Write '0xF' to the advertised characteristic: {my_char_uuid}")
     if trigger.__module__ == "threading":
         trigger.wait()
     else:
         await trigger.wait()
+
     await asyncio.sleep(2)
     logger.debug("Updating")
-    server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B").value = bytearray(
-        b"i"
-    )
-    server.update_value(
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD", "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    )
+    server.get_characteristic(my_char_uuid)
+    server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
     await asyncio.sleep(5)
     await server.stop()
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run(loop))
